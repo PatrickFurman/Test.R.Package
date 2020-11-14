0c7e12dc59abc7ba36c20e850f3a89b0541d730e
PatrickFurman <patrickfurman@gmail.com>
2020-11-14 14:05:31 -0800

    moved library statement

--- R/f.R
+++ R/f.R
@@ -1,10 +1,11 @@
-library(parallel)
+
 
 f = function(x) {
   return(sample(LETTERS,1))
 }
 
 f_in_parallel = function(x) {
+  library(parallel)
   num_cores = detectCores()
   cl = makeCluster(num_cores - 1)
 
@@ -18,6 +19,7 @@ f_in_parallel = function(x) {
 }
 
 f_not_parallel = function(x) {
+  library(parallel)
   start = Sys.time()
   y = lapply(1:1000000, f)
   end = Sys.time()
